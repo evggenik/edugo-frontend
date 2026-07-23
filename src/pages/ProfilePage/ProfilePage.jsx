ec2bd1c13edd571fd5943f5d554a1767712c4a1b
Evgeny Buchelnikov <gru554@yandex.ru>
2026-07-23 23:03:06 +1000

    Remove <Header> in ProfilePage

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -2,13 +2,12 @@ import Header from "../../components/Header/Header";
 
 function ProfilePage() {
     return (
-        <>
-            <Header />
-            <div style={{ padding: '24px' }}>
-                <h1>Профиль</h1>
-                <p>Здесь будет управление профилем (пока не реализовано)</p>
-            </div>
-        </>
+        
+        <div style={{ padding: '24px' }}>
+            <h1>Профиль</h1>
+            <p>Здесь будет управление профилем (пока не реализовано)</p>
+        </div>
+        
     )
 }
 
